6e2ecc2f7fb137e32b6beddb844d4d30fd2f8d66
janfala <janfalarowski@web.de>
2024-03-30 17:55:01 +0100

    improved code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import Searchbar from "./Searchbar";
 
 function App() {
-  return <Searchbar />;
+  return (
+    <div>
+      <h1>Where To Watch</h1>
+      <div>
+        <h2>How to use</h2>
+        <p className="info-text">Find out where you can watch your favourite title by searching for it down below</p>
+      </div>
+      <h2>Search</h2>
+      <Searchbar />;
+    </div>
+  );
 }
 
 export default App;
--- src/Searchbar.js
+++ src/Searchbar.js
@@ -2,44 +2,21 @@ import { useState, useEffect } from "react";
 
 const Searchbar = () => {
   const API_KEY = process.env.REACT_APP_KEY;
-  const [message, setMessage] = useState(null);
-  const [url, setUrl] = useState(null);
-  const [movies, setMovies] = useState(null);
+  let url = "";
 
-  const searchMovie = () => {
-    const inputField = document.getElementById("field");
-    !inputField.value ? setMessage("Please input a movie name") : getMovie(inputField.value);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    let input = e.target.elements["title"].value;
+    //  url = "https://api.watchmode.com/v1/autocomplete-search/?apiKey=${API_KEY}&search_value=${input}&search_type=2`";
+    url = "http://localhost:8000/movies";
   };
-
-  const getMovie = (input) => {
-    // setUrl(`https://api.watchmode.com/v1/autocomplete-search/?apiKey=${API_KEY}&search_value=${input}&search_type=3`);
-    setUrl("http://localhost:8000/movies");
-  };
-
-  useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("failed to fetch data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setMovies(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, [url]);
 
   return (
     <div>
-      <div className="content">
-        <input type="text" placeholder="A new Hope.." id="field" />
-        <button onClick={() => searchMovie()}>Search</button>
-      </div>
-      {message && <p id="message">{message}</p>}
+      <form className="content" onSubmit={handleSearch}>
+        <input type="text" name="title" placeholder="A new Hope.." required />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 };
